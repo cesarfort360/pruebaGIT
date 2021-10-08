37b0e4d3b30a42fa57b7667c10dedf250b897fd9
cesarfort360 <92091250+cesarfort360@users.noreply.github.com>
2021-10-08 09:57:28 +0200

    Update EjemploGIT.java

--- src/pruebaGIT/EjemploGIT.java
+++ src/pruebaGIT/EjemploGIT.java
@@ -5,8 +5,7 @@ public class EjemploGIT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
-		String nombre = "César";
-		int edad = 22;
+		String nombre = "CÃ©sar";
 	}
 
 }
